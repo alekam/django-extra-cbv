f08bccce7bcd1f5c135ed0970164083814757ad5
Alex Kamedov <alex@kamedov.ru>
2013-12-25 20:40:31 +0600

    fix

--- extra_cbv/responses.py
+++ extra_cbv/responses.py
@@ -1,9 +1,11 @@
 from .utils import LazyEncoder
+from django.http.response import HttpResponse
+import json
 
 
 class JsonResponse(HttpResponse):
     def __init__(self, data):
         HttpResponse.__init__(
-            self, content=simplejson.dumps(data, cls=LazyEncoder),
+            self, content=json.dumps(data, cls=LazyEncoder),
             content_type="application/json",
         )
